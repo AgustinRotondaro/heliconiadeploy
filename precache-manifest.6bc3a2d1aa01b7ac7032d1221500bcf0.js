d120428f3b7b5ff3bea90a0079fa6db0aae35be3
Agustin Rotondaro <you@example.com>
2020-07-14 20:09:18 -0300

    Updates

---
+++ precache-manifest.6bc3a2d1aa01b7ac7032d1221500bcf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51fda93e5bb171059ffc3dbb074b89aa",
+    "url": "/heliconiadeploy/index.html"
+  },
+  {
+    "revision": "a5c8ccb06af2d3720625",
+    "url": "/heliconiadeploy/static/css/main.fb5e79b7.chunk.css"
+  },
+  {
+    "revision": "f8fba5f0d5aeac60152d",
+    "url": "/heliconiadeploy/static/js/2.cad3bf64.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/heliconiadeploy/static/js/2.cad3bf64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c8ccb06af2d3720625",
+    "url": "/heliconiadeploy/static/js/main.a9a0be94.chunk.js"
+  },
+  {
+    "revision": "8d00f8d09b16c7e06185",
+    "url": "/heliconiadeploy/static/js/runtime-main.6c82cf30.js"
+  }
+]);
